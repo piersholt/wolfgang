37fce814d15293c176daadd23878464c767a0193
Piers Holt <piersholt@gmail.com>
2018-11-15 16:49:20 +1100

    Remove obsolete methods from device interface

--- lib/bluez-dbus/interfaces/bluez_device.rb
+++ lib/bluez-dbus/interfaces/bluez_device.rb
@@ -1,22 +1,4 @@
 module BluezDevice
-  def connect
-  end
-
-  def disconnect
-  end
-
-  def connect_profile(uuid)
-  end
-
-  def disconnect_profile(uuid)
-  end
-
-  def pair
-  end
-
-  def cancel_pairing
-  end
-
   # PROPERTIES
 
   def connected?
