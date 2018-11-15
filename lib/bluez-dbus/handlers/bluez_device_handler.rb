fa1b4908dab424b25069091fc95408cac601f528
Piers Holt <piersholt@gmail.com>
2018-11-15 16:50:40 +1100

    Add interface state to handler logging

--- lib/bluez-dbus/handlers/bluez_device_handler.rb
+++ lib/bluez-dbus/handlers/bluez_device_handler.rb
@@ -4,6 +4,7 @@ class BluezDeviceHandler < BluezBaseHandler
   end
 
   def handle(signal, args)
-    LOGGER.info("#{self.class}") { "#{signal}: #{args}" }
+    LOGGER.info("#{self.class}") { "#{signal}: #{args.find_all { |k,_| k != :state }}" }
+    LOGGER.info("#{self.class}") { "State: #{args[:state]}" }
   end
 end
