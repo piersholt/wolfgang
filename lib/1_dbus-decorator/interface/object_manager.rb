8b40d25d934c168eb0c55e1f5aad7e514ab6fc96
Piers Holt <piersholt@gmail.com>
2018-11-16 15:33:15 +1100

    Remove obsolete ObjectManager methods

--- lib/1_dbus-decorator/interface/object_manager.rb
+++ lib/1_dbus-decorator/interface/object_manager.rb
@@ -11,21 +11,6 @@
 module ObjectManager
   OBJECT_MANAGER_INTERFACE = 'org.freedesktop.DBus.ObjectManager'
 
-  def managed_objects
-    result = object_manager_interface.GetManagedObjects
-    # raise StandardError('get managed objects length is > 1') unless result.length == 1
-    # result = result.first
-
-    mapped_objects = result.map do |object, interfaces|
-      object_interfaces = interfaces.keys
-      object_interfaces.sort!
-      [object, object_interfaces]
-    end
-
-    mapped_objects.sort! { |a, b| a[0] <=> b[0] }
-    mapped_objects.to_h
-  end
-
   # private
 
   def get_managed_objects
