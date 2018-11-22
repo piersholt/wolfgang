96d5a011d09397b28e47446157b83163f01fbef6
Piers Holt <piersholt@gmail.com>
2018-11-22 16:49:03 +1100

    Remove deprecated source files

--- lib/2_bluez-dbus/_deprecated/bluez_adapter_handler.rb
+++
@@ -1,9 +0,0 @@
-class BluezAdapterHandler < BluezBaseHandler
-  def initialize(target = BLUEZ_ADAPTER)
-    super(target)
-  end
-
-  def handle(signal, args)
-    LOGGER.info("#{self.class}") { "#{signal}: #{args}" }
-  end
-end
--- lib/2_bluez-dbus/_deprecated/bluez_base_handler.rb
+++
@@ -1,7 +0,0 @@
-class BluezBaseHandler
-  attr_reader :target
-
-  def initialize(target)
-    @target = target
-  end
-end
