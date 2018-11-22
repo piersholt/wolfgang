cd1315dba4a726b6bdbef779fb287cad3212ca6d
Piers Holt <piersholt@gmail.com>
2018-11-22 16:55:15 +1100

    Clean up Bluez Manager dependency file

--- lib/3_bluez-manager/bluez-manager.rb
+++ lib/3_bluez-manager/bluez-manager.rb
@@ -2,18 +2,7 @@
 
 bluez_manager_root = '3_bluez-manager'
 
-
-
 require "#{bluez_manager_root}/profiles/profiles"
-
 require "#{bluez_manager_root}/api/bluez_client_api"
-
-# require "#{bluez_manager_root}/listeners/bluez_base_listener"
-# require "#{bluez_manager_root}/listeners/bluez_player_listener"
-# require "#{bluez_manager_root}/listeners/bluez_device_listener"
-# require "#{bluez_manager_root}/listeners/bluez_controller_listener"
-# require "#{bluez_manager_root}/listeners/bluez_root_listener"
-# require "#{bluez_manager_root}/listeners/bluez_service_listener"
-
-require "#{bluez_manager_root}/bluez_interface"
+require "#{bluez_manager_root}/bluez_dbus_interface"
 require "#{bluez_manager_root}/bluez_manager"
