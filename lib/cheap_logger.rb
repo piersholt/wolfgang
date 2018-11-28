fbc6877552860e33260c9e221874529b24de2f91
Piers Holt <piersholt@gmail.com>
2018-11-28 12:38:51 +1100

    Refactor Logger for better ASCII colour management

--- lib/cheap_logger.rb
+++ lib/cheap_logger.rb
@@ -1,15 +1,54 @@
 require 'logger'
 
+RESET = '0'
+LIGHT_GRAY = '37'
+GREEN = '32'
+YELLOW = '33'
+RED = '31'
+MAGENTA = '35'
+
+
+def gray
+  "\e[#{LIGHT_GRAY}m"
+end
+
+def green
+  "\e[#{GREEN}m"
+end
+
+def yellow
+  "\e[#{YELLOW}m"
+end
+
+def red
+  "\e[#{RED}m"
+end
+
+def magenta
+  "\e[#{MAGENTA}m"
+end
+
+def clear
+  "\e[#{RESET}m"
+end
+
 def get_logger
   l = Logger.new(STDOUT)
   l.formatter = proc do |severity, time, progname, msg|
-      severity_to_colour_map = {'DEBUG'=>'0;37', 'INFO'=>'32', 'WARN'=>'33', 'ERROR'=>'31', 'FATAL'=>'31', 'UNKNOWN'=>'37'}
-      # severity_to_abrv_map = { 'DEBUG': 'D', 'INFO': 'I', 'WARN': 'W', 'ERROR': 'E', 'FATAL': 'F', 'UNKNOWN': '?'}
+      severity_to_colour_map = {
+        'DEBUG' => gray,
+        'INFO' => green,
+        'WARN' => yellow,
+        'ERROR' => red,
+        'FATAL' => red,
+        'UNKNOWN' => magenta,
+        'ANY' => magenta,
+      }
       color = severity_to_colour_map[severity]
 
       formatted_severity = sprintf("%-5s",severity)
       formatted_progname = sprintf("%20s",progname)
-      formatted_progname_string =  "[#{formatted_progname}]"
+      # formatted_progname_string =  "[#{formatted_progname}]"
 
 
       # formatted_time = time.strftime("%y-%m-%d %H:%M:%S.") << time.usec.to_s[0..2].rjust(3)
@@ -18,8 +57,11 @@ def get_logger
 
       msg.strip! rescue StandardError
 
-      #"\033[0;37m#{formatted_time}\033[0m [\033[#{color}m#{formatted_severity}\033[0m] #{msg.strip} (pid:#{$$})\n"
-      m = "\033[0;37m#{formatted_time}\033[0m [\033[#{color}m#{formatted_severity}\033[0m] #{formatted_progname_string} #{msg}"
+      #"\e[0;37m#{formatted_time}\e[0m [\e[#{color}m#{formatted_severity}\e[0m] #{msg.strip} (pid:#{$$})\n"
+      m = "#{gray}#{formatted_time}#{clear} "\
+          "[#{color}#{formatted_severity}#{clear}] "\
+          "[#{formatted_progname}#{clear}] "\
+          "#{msg}#{clear}"
       m.concat("\n") unless severity == Logger::UNKNOWN
     end
 
