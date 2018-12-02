2fbcaa7eabef5068725745ec1a554fc822957d96
Piers Holt <piersholt@gmail.com>
2018-12-03 09:54:26 +1100

    Tweak Profile/Service classes

--- lib/3-bluez-manager/profiles/a2dp.rb
+++ lib/3-bluez-manager/profiles/a2dp.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
+# Comment
 module Profiles::A2DP
+  PROFILE = {
+    service_class_name: 'AdvancedAudioDistribution',
+    specification: 'Advanced Audio Distribution Profile (A2DP)',
+    allowed_usage: 'Profile Class'
+  }.freeze
+
   SINK = {
     service_class_name: 'AudioSink',
     specification: 'Advanced Audio Distribution Profile (A2DP)',
@@ -10,10 +17,5 @@ module Profiles::A2DP
     service_class_name: 'AudioSource',
     specification: 'Advanced Audio Distribution Profile (A2DP)',
     allowed_usage: 'Service Class'
-  }.freeze
-  PROFILE = {
-    service_class_name: 'AdvancedAudioDistribution',
-    specification: 'Advanced Audio Distribution Profile (A2DP)',
-    allowed_usage: 'Profile Class'
   }.freeze
 end
--- lib/3-bluez-manager/profiles/avrcp.rb
+++ lib/3-bluez-manager/profiles/avrcp.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
+# Comment
 module Profiles::AVRCP
   PROFILE = {
     service_class_name: 'A/V_RemoteControl',
     specification: 'Audio/Video Remote Control Profile (AVRCP)',
     allowed_usage: 'Service Class/Profile'
   }.freeze
+
   TARGET = {
     service_class_name: 'A/V_RemoteControlTarget',
     specification: 'Audio/Video Remote Control Profile (AVRCP)',
