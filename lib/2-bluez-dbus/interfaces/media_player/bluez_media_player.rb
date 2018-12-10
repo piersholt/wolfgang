cdbc9ff687bdd91347dfb975a3613014d37d866b
Piers Holt <piersholt@gmail.com>
2018-12-11 08:14:56 +1100

    Split BluezMediaPlayer methods and properties into modules

--- lib/2-bluez-dbus/interfaces/media_player/bluez_media_player.rb
+++ lib/2-bluez-dbus/interfaces/media_player/bluez_media_player.rb
@@ -3,44 +3,8 @@
 # Bluez Interface: org.bluez.MediaPlayer1
 module BluezMediaPlayer
   include InterfaceConstants
-  # METHODS
-  def play
-    interface(BLUEZ_MEDIA_PLAYER).Play do |resp|
-      LOGGER.info(self.class) { "#Play callback? #{resp}" }
-    end
-  end
-
-  def pause
-    interface(BLUEZ_MEDIA_PLAYER).Pause do |resp|
-      LOGGER.info(self.class) { "#Pause callback? #{resp}" }
-    end
-  end
-
-  def stop
-    interface(BLUEZ_MEDIA_PLAYER).Stop
-  end
-
-  def next
-    interface(BLUEZ_MEDIA_PLAYER).Next do |resp|
-      LOGGER.info(self.class) { "#Next callback? #{resp}" }
-    end
-  end
-
-  def previous
-    interface(BLUEZ_MEDIA_PLAYER).Previous do |resp|
-      LOGGER.info(self.class) { "#Previous callback? #{resp}" }
-    end
-  end
-
-  def fast_forward
-    interface(BLUEZ_MEDIA_PLAYER).FastForward
-  end
-
-  def rewind
-    interface(BLUEZ_MEDIA_PLAYER).Rewind
-  end
-
-  # PROPERTIES
+  include Methods
+  include Properties
 
   def media_player
     self.default_iface = BLUEZ_MEDIA_PLAYER
---
+++ lib/2-bluez-dbus/interfaces/media_player/properties.rb
@@ -0,0 +1,86 @@
+# frozen_string_literal: true
+
+# Bluez Interface: org.bluez.MediaPlayer1
+module BluezMediaPlayer
+  # Comment
+  module Properties
+    def track
+      media_player_property('Track')
+    end
+
+    # Playber object path
+    def device
+      media_player_property('Device')
+    end
+
+    # Name of player, i.e. Sptify
+    def name
+      media_player_property('Name')
+    end
+
+    # * Major Player Type
+    # -- Audio
+    # -- Video
+    # -- Broadcast Audio
+    # -- Broadcast Video
+    def type
+      media_player_property('Type')
+    end
+
+    # * Player Sub Type
+    # -- Audio Book
+    # -- Postcast
+    def subtype
+      media_player_property('Subtype')
+    end
+
+    # If present indicates the player can be browsed using MediaFolder interface.
+    # @browseable
+    def browseable
+      media_player_property('Browseable')
+    end
+
+    # If present indicates the player can be searched using MediaFolder interface.
+    # @searchable
+    def searchable
+      media_player_property('Searchable')
+    end
+
+    # Playlist object path.
+    # @playlist
+    def playlist
+      media_player_property('Playlist')
+    end
+
+    # @Equalizer [readwrite]
+    # Possible values: "off" or "on"
+    def equalizer
+      media_player_property('Equalizer')
+    end
+
+    # @Repeat [readwrite]
+    # Possible values: "off", "singletrack", "alltracks" or "group"
+    def repeat
+      media_player_property('Repeat')
+    end
+
+    # @Shuffle [readwrite]
+    # Possible values: "off", "alltracks" or "group"
+    def shuffle
+      media_player_property('Shuffle')
+    end
+
+    # @Scan [readwrite]
+    # Possible values: "off", "alltracks" or "group"
+    def scan
+      media_player_property('Scan')
+    end
+
+    # @Status [readonly]
+    # Possible status: "playing", "stopped", "paused", "forward-seek",
+    # "reverse-seek" or "error"
+    def status
+      media_player_property('Status')
+    end
+  end
+end
