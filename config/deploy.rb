d922e611b0d08691bbc95f42f33978082077bbf6
Piers Holt <piersholt@gmail.com>
2019-05-21 15:29:36 +1000

    Migrate Capistrano deployment tasks to use hostnames

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,3 +37,5 @@ set :deploy_to, "/home/pi/#{fetch(:application)}"
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+set :address_pi, 'pi.local'
