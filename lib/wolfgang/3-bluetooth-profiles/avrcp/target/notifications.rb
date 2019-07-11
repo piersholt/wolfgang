89bbf1c1c28bf0f595489be14a34647c1627177b
Piers Holt <piersholt@gmail.com>
2019-07-11 10:24:36 +1000

    Update logging

--- lib/wolfgang/3-bluetooth-profiles/avrcp/target/notifications.rb
+++ lib/wolfgang/3-bluetooth-profiles/avrcp/target/notifications.rb
@@ -7,21 +7,27 @@ module Wolfgang
       module Notifications
         attr_accessor :notifications_queue
 
+        MODULE_PROG = 'Target::Notifications'
+
+        LOG_PLAYER_ADDED = 'Player added!'
+        LOG_PLAYER_CHANGED = 'Player changed'
+        LOG_PLAYER_REMOVED = 'Player removed!'
+        LOG_ATTRIBUTES_CHANGED = 'Player attributes changed!'
 
         def player_added!
-          LogActually.avrcp.info('Notification') { 'Player added!' }
+          LogActually.avrcp.info(MODULE_PROG) { LOG_PLAYER_ADDED }
           n = Yabber::Notification.new(topic: :target, name: :player_added, properties: addressed_player.object.path)
           notifications_queue.push(n)
         end
 
         def player_changed!
-          LogActually.avrcp.info('Notification') { 'Player changed!!' }
+          LogActually.avrcp.info(MODULE_PROG) { LOG_PLAYER_CHANGED }
           n = Yabber::Notification.new(topic: :target, name: :player_changed, properties: addressed_player.object.path)
           notifications_queue.push(n)
         end
 
         def player_removed!
-          LogActually.avrcp.info('Notification') { 'Player removed!' }
+          LogActually.avrcp.info(MODULE_PROG) { LOG_PLAYER_REMOVED }
           n = Yabber::Notification.new(topic: :target, name: :player_removed)
           notifications_queue.push(n)
         end
