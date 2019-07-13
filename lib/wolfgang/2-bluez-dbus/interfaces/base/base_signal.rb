2ae7e7da1ef3d465e1db746f0444700a52c23f17
Piers Holt <piersholt@gmail.com>
2019-07-13 10:49:00 +1000

    BaseSignal constants and formatting

--- lib/wolfgang/2-bluez-dbus/interfaces/base/base_signal.rb
+++ lib/wolfgang/2-bluez-dbus/interfaces/base/base_signal.rb
@@ -22,10 +22,14 @@ module Wolfgang
     include ObjectConstants
     include ObjectHelpers
 
-    attr_reader :sender, :path, :interface, :member
-    alias_method :signal, :member
+    SIGNAL_SENDER = 'Bluez'
+    SIGNAL_PATH   = 'whatever'
+    FILTER_BLUEZ  = 'bluez'
 
-    def initialize(sender: 'Bluez', path: 'whatever', interface:, member: )
+    attr_reader :sender, :path, :interface, :member
+    alias signal member
+
+    def initialize(sender: SIGNAL_SENDER, path: SIGNAL_PATH, interface:, member: )
       @sender = sender
       @interface = interface
       @member = member
@@ -33,13 +37,15 @@ module Wolfgang
     end
 
     def filter_non_bluez_hash(interface_hash)
-      result = interface_hash.find_all { |name, props| name.include?('bluez') }
+      result = interface_hash.find_all do |name, _|
+        name.include?(FILTER_BLUEZ)
+      end
       return result if result.empty?
       result.to_h
     end
 
     def filter_non_bluez_array(interface_name_array)
-      interface_name_array.find_all { |str| str.include?('bluez') }
+      interface_name_array.find_all { |str| str.include?(FILTER_BLUEZ) }
     end
   end
 end
