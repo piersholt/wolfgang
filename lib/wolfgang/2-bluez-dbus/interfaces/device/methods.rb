0bba5f135bba33a08152410799b2ab63030db346
Piers Holt <piersholt@gmail.com>
2019-07-15 11:09:57 +1000

    Add ConnectProfile, DisconnectProfile, Pair, and CancelPairing methods to BluezDeviceInterface

--- lib/wolfgang/2-bluez-dbus/interfaces/device/methods.rb
+++ lib/wolfgang/2-bluez-dbus/interfaces/device/methods.rb
@@ -16,15 +16,27 @@ module Wolfgang
         device_interface.Disconnect(&default_callback)
       end
 
-      # @argument: uuid String
-      def connect_profile(uuid); end
+      # @param String uuid
+      def connect_profile(uuid)
+        called(BLUEZ_DEVICE, :connect_profile, uuid: uuid)
+        device_interface.ConnectProfile(uuid, &default_callback)
+      end
 
-      # @argument: uuid String
-      def disconnect_profile(uuid); end
+      # @param String uuid
+      def disconnect_profile(uuid)
+        called(BLUEZ_DEVICE, :disconnect_profile, uuid: uuid)
+        device_interface.DisconnectProfile(uuid, &default_callback)
+      end
 
-      def pair; end
+      def pair
+        called(BLUEZ_DEVICE, :pair)
+        device_interface.Pair(&default_callback)
+      end
 
-      def cancel_pairing; end
+      def cancel_pairing
+        called(BLUEZ_DEVICE, :cancel_pairing)
+        device_interface.CancelPairing(&default_callback)
+      end
     end
   end
 end
