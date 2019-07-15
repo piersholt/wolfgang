41bc02eb14ee6ba5eeb16fecbb7c4f3b4977e630
Piers Holt <piersholt@gmail.com>
2019-07-15 11:37:02 +1000

    Reorganise Manage::SignalHandling

--- lib/wolfgang/3-bluetooth-profiles/core/manager/signal_handling.rb
+++ lib/wolfgang/3-bluetooth-profiles/core/manager/signal_handling.rb
@@ -29,23 +29,7 @@ module Wolfgang
           end
         end
 
-        # ---------------------------------------------------------------------
-
-        def create_or_update_device(signal)
-          device_path = signal.path
-          case devices.key?(device_path)
-          when true
-            devices[device_path].attributes!(signal.changed)
-            devices[device_path]
-          when false
-            new_device = Core::Device.new(device_path)
-            new_device.attributes!(signal.changed)
-            devices[device_path] = new_device
-            devices[device_path]
-          end
-        rescue StandardError => e
-          with_backtrace(logger, e)
-        end
+        # DEVICE_PROPERTIES_CHANGED_BLOCK -------------------------------------
 
         def device_properties_changed(signal)
           logger.debug(self.class) { "device_properties_changed! #{signal.path}" }
@@ -64,9 +48,40 @@ module Wolfgang
           with_backtrace(logger, e)
         end
 
-        # Manager calls device_connecting! upon receiving :connect as the
-        # connecting/disconnecting states are used for UI 'loading' states and
-        # it would be rather counter intuitive to wait for device signal.
+        def create_or_update_device(signal)
+          device_path = signal.path
+          case devices.key?(device_path)
+          when true
+            devices[device_path].attributes!(signal.changed)
+            devices[device_path]
+          when false
+            new_device = Core::Device.new(device_path)
+            new_device.attributes!(signal.changed)
+            devices[device_path] = new_device
+            devices[device_path]
+          end
+        rescue StandardError => e
+          with_backtrace(logger, e)
+        end
+
+        def device_connected(device_in_question)
+          logger.debug(self.class) { 'Device connected!' }
+          device_connected!(device_in_question)
+        end
+
+        def device_disconnected(device_in_question)
+          logger.debug(self.class) { 'Device disconnected!' }
+          device_disconnected!(device_in_question)
+        end
+
+        # DEVICE_INTERFACE_CALLED_BLOCK ---------------------------------------
+
+        # The :connecting and :disconnecting notifications are used to update
+        # the UI state. As to make the UI as responsible as possible,
+        # Core::Manager will send the notifications when #connect or
+        # #disconnect is called rather than waiting for the InterfaceCalled
+        # signal.
+
         def device_interface_called(interface_called_signal)
           logger.debug(self.class) do
             "#device_interface_called: (#{interface_called_signal})"
@@ -92,16 +107,6 @@ module Wolfgang
           end
           # properties = find_by(:address, address) || {}
           # device_disconnecting!(properties)
-        end
-
-        def device_connected(device_in_question)
-          logger.debug(self.class) { 'Device connected!' }
-          device_connected!(device_in_question)
-        end
-
-        def device_disconnected(device_in_question)
-          logger.debug(self.class) { 'Device disconnected!' }
-          device_disconnected!(device_in_question)
         end
       end
     end
