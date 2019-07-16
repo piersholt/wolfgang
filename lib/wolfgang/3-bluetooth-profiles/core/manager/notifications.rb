f85b91761bd6f97fd941120c6c10a863015df8ed
Piers Holt <piersholt@gmail.com>
2019-07-16 11:40:53 +1000

    Core::Manager::Notifications logging constants

--- lib/wolfgang/3-bluetooth-profiles/core/manager/notifications.rb
+++ lib/wolfgang/3-bluetooth-profiles/core/manager/notifications.rb
@@ -7,26 +7,33 @@ module Wolfgang
       module Notifications
         attr_accessor :notifications_queue
 
+        LOG_DEVICE_CONNECTING    = 'Device connecting!'
+        LOG_DEVICE_DISCONNECTING = 'Device disconnecting!'
+        LOG_DEVICE_CONNECTED     = 'Device connected!'
+        LOG_DEVICE_DISCONNECT    = 'Device disconnected!'
+
+        MODULE_PROG = 'Manager::Notifications'
+
         def device_connecting!(device_in_question)
-          LogActually.core.info('Notification') { 'Device connecting!' }
+          LogActually.core.info(MODULE_PROG) { LOG_DEVICE_CONNECTING }
           n = Yabber::Notification.new(topic: :device, name: :device_connecting, properties: device_in_question.attributes)
           notifications_queue.push(n)
         end
 
         def device_disconnecting!(device_in_question)
-          LogActually.core.info('Notification') { 'Device disconnecting!' }
+          LogActually.core.info(MODULE_PROG) { LOG_DEVICE_DISCONNECTING }
           n = Yabber::Notification.new(topic: :device, name: :device_disconnecting, properties: device_in_question.attributes)
           notifications_queue.push(n)
         end
 
         def device_connected!(device_in_question)
-          LogActually.core.info('Notification') { 'Device connected!' }
+          LogActually.core.info(MODULE_PROG) { LOG_DEVICE_CONNECTED }
           n = Yabber::Notification.new(topic: :device, name: :device_connected, properties: device_in_question.attributes)
           notifications_queue.push(n)
         end
 
         def device_disconnected!(device_in_question)
-          LogActually.core.info('Notification') { 'Device disconnected!' }
+          LogActually.core.info(MODULE_PROG) { LOG_DEVICE_DISCONNECT }
           n = Yabber::Notification.new(topic: :device, name: :device_disconnected, properties: device_in_question.attributes)
           notifications_queue.push(n)
         end
