467466196ddcfd63b856567ea0c003199e001eb6
Piers Holt <piersholt@gmail.com>
2019-07-16 11:36:01 +1000

    White spacing

--- lib/wolfgang/2-bluez-dbus/interfaces/object_manager/signals/interfaces_added.rb
+++ lib/wolfgang/2-bluez-dbus/interfaces/object_manager/signals/interfaces_added.rb
@@ -8,34 +8,36 @@ module Wolfgang
         attr_reader :object, :added_interfaces
 
         def initialize(object, added_interfaces)
-          super(interface: ObjectManager::OBJECT_MANAGER,
-            member:   'InterfacesAdded')
-            @object = object
-            @added_interfaces = added_interfaces
-          end
+          super(
+            interface: ObjectManager::OBJECT_MANAGER,
+            member:   'InterfacesAdded'
+          )
+          @object = object
+          @added_interfaces = added_interfaces
+        end
 
-          # @deprecated
-          def changes
-            added_interfaces
-          end
+        # @deprecated
+        def changes
+          added_interfaces
+        end
 
-          # @deprecated
-          def object_path
-            object
-          end
+        # @deprecated
+        def object_path
+          object
+        end
 
-          def filtered_interfaces
-            filter_non_bluez_hash(added_interfaces)
-          end
+        def filtered_interfaces
+          filter_non_bluez_hash(added_interfaces)
+        end
 
-          def interface_names
-            filter_non_bluez_array(added_interfaces.keys)
-          end
+        def interface_names
+          filter_non_bluez_array(added_interfaces.keys)
+        end
 
-          def object_suffixed
-            interface_suffixed(object)
-          end
+        def object_suffixed
+          interface_suffixed(object)
         end
       end
     end
+  end
 end
