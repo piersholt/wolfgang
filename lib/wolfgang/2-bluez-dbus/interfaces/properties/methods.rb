610084265c487ce5d315840ea2a860bb53241ff6
Piers Holt <piersholt@gmail.com>
2019-07-26 13:03:16 +1000

    Properties interface methods block support

--- lib/wolfgang/2-bluez-dbus/interfaces/properties/methods.rb
+++ lib/wolfgang/2-bluez-dbus/interfaces/properties/methods.rb
@@ -7,21 +7,22 @@ module Wolfgang
       include InterfaceConstants
       PROG = 'Properties'
 
-      def property_get(interface_name = selected_interface, property_name)
-        logger.debug(respond_to?(:prog) ? prog : PROG) { "#property_get(#{interface_name}, #{property_name})" }
-        properties_interface.Get(interface_name, property_name)
+      def property_get(interface_name = selected_interface, property_name, &block)
+        logger.debug(respond_to?(:prog) ? prog : PROG) { "#property_get(#{interface_name}, #{property_name}, &block=#{block_given?})" }
+        properties_interface.Get(interface_name, property_name, &block)
       end
 
       # @desc:  Sets the specified property. Similar to Get, the interface_name
       # argument may be ''.
+      # @todo add &block
       def property_set(interface_name = selected_interface, property_name, value)
         properties_interface.Set(interface_name, property_name, value)
       end
 
       # @return: a dictionary of string property names to variant values.
-      def property_get_all(interface_name = selected_interface)
-        logger.debug(respond_to?(:prog) ? prog : PROG) { "#property_get_all(#{interface_name})" }
-        properties_interface.GetAll(interface_name)
+      def property_get_all(interface_name = selected_interface, &block)
+        logger.debug(respond_to?(:prog) ? prog : PROG) { "#property_get_all(#{interface_name}, &block=#{block_given?})" }
+        properties_interface.GetAll(interface_name, &block)
       end
     end
   end
